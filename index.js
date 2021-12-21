048f206034e0e71b1de4a1ac948e2b4d35501ce4
Benni0501 <benni.kogler@gmail.com>
2021-12-21 22:35:27 +0100

    added comments

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Root-Verzeichnis für Website --> vielleicht auf Apache übertragen
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,'index.html'));
     console.log("Get");
    // res.end();
 })
 
+// mögliche Ansteuerung von Website per POST-Request
 app.post("/writeSomething", (req,res) => {
     console.log(req.body.textToWrite);
     res.status(200);
     res.end();
 })
 console.log("Trying to start server");
+// Server Start
 app.listen(3001, () => console.log("server started successfully"));
