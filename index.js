b70ce477a55249fe55505b660281e3f71a652c0a
Benni0501 <benni.kogler@gmail.com>
2021-12-21 19:49:49 +0100

    made a few tests

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const fs = require('fs');
 const app = express();
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get("/", (req,res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname,'index.html'));
+    console.log("Get");
+   // res.end();
 })
 console.log("Trying to start server");
 app.listen(3001, () => console.log("server started successfully"));
