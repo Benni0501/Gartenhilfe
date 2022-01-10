726c03646726a17948d4c8e5a9ce3b5ca997fa33
Benjamin <kogbeb19@htl-kaindorf.at>
2022-01-10 11:20:43 +0100

    Added mqtt

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const fs = require('fs');
 const app = express();
 const path = require('path');
+const mqtt = require('mqtt');
+const host = 'broker.hivemq.com'
+const port = '1883'
+const clientId = 'mqtt_123'
+const connectUrl = 'mqtt://broker.hivemq.com:1883'
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 
 //Test-Counter
 var counter = 0;
+
+/*const client = mqtt.connect(connectUrl , {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    reconnectPeriod: 1000
+})
+const topic = 'nodejs/mqtt'
+client.on('connect', () => {
+    console.log("Connected");
+    client.subscribe([topic], () => {
+        console.log("Subscribed")
+    })
+    client.on('message', (topic, payload) => {
+        console.log('Received Message:', topic, payload.toString())
+      })
+    client.publish("nodejs/test", 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error(error)
+        }
+    })
+})*/
 
 // Root-Verzeichnis für Website --> vielleicht auf Apache übertragen
 app.get("/", (req,res) => {
