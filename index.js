b6e34115061d5a035fd83bed57cd8a3a05d4bbad
Benjamin <kogbeb19@htl-kaindorf.at>
2022-01-11 10:30:45 +0100

    did some mqtt tests

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 const mqtt = require('mqtt');
-const host = 'broker.hivemq.com'
-const port = '1883'
 const clientId = 'mqtt_123'
-const connectUrl = 'mqtt://broker.hivemq.com:1883'
+const connectUrl = 'mqtt://192.168.43.129:1883'
 
 app.use(cors());
 app.use(express.json());
@@ -17,13 +15,13 @@ app.use(express.urlencoded({ extended: false }));
 var counter = 0;
 
 
-/*const client = mqtt.connect(connectUrl , {
+const client = mqtt.connect(connectUrl , {
     clientId,
     clean: true,
     connectTimeout: 4000,
     reconnectPeriod: 1000
 })
-const topic = 'nodejs/mqtt'
+const topic = "webthings/aa3cc4d8-3d99-49f6-875f-6deb8ea7c2f5/actions/on"
 client.on('connect', () => {
     console.log("Connected");
     client.subscribe([topic], () => {
@@ -32,12 +30,14 @@ client.on('connect', () => {
     client.on('message', (topic, payload) => {
         console.log('Received Message:', topic, payload.toString())
       })
-    client.publish("nodejs/test", 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    client.publish("webthings/aa3cc4d8-3d99-49f6-875f-6deb8ea7c2f5/properties/on/set", "true", { qos: 2, retain: false }, (error) => {
         if (error) {
           console.error(error)
+        } else {
+            console.log("Sent");
         }
     })
-})*/
+})
 
 // Root-Verzeichnis für Website --> vielleicht auf Apache übertragen
 app.get("/", (req,res) => {
